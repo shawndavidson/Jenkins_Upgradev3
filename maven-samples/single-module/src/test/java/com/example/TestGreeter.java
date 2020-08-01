a010f819a809d744ae7ca8ffd023746f3c9e5430
Shawn Davidson <shawnkdavidson@gmail.com>
2020-08-01 06:36:37 +0100

    Added silly test

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -37,4 +37,11 @@ public class TestGreeter {
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
+
+    @Test
+  public void greetShouldIncludeGreetingMessage() {
+    String someone = "World, Many thanks!";
+
+    assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
+  }
 }
